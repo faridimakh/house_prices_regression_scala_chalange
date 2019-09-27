b78855ee6013a1dbcbf2dd809a0c734440e26206
faridimakh <secintfar1A>
2019-09-27 14:23:34 +0200

    kmean regularisation

--- src/main/scala/packfar/kagglemain.scala
+++ src/main/scala/packfar/kagglemain.scala
@@ -1,6 +1,5 @@
 package packfar
 
-import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.regression.RandomForestRegressor
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.{FloatType, IntegerType}
@@ -14,38 +13,56 @@ object kagglemain extends App {
   val df_brut_test: DataFrame = beginspark.importDF("Data/test.csv") //to get data consistency between train and test
 
   val df_num_test_kmean = df_brut_test.select_cols_By_Type(IntegerType)
-    //    .withColumn("SalePrice", lit(0))
     .drop("Id", "YearBuilt", "YearRemodAdd", "YrSold")
     .conveting_multiple_columns_to_specific_DataType(FloatType)
-    .impute_numeric_vars_with_mean_or_median()
+
+  val df_cat_test_kmean = df_brut_test.select_Impute_And_Transform_categorical_to_numerical()
+    .conveting_multiple_columns_to_specific_DataType(FloatType)
+  val df1 = df_num_test_kmean.join_df2_by_index(df_cat_test_kmean)
+
 
   val df_num_train_kmean = df_brut_train.select_cols_By_Type(IntegerType)
     .drop("Id", "YearBuilt", "YearRemodAdd", "YrSold")
     .conveting_multiple_columns_to_specific_DataType(FloatType)
     .select_cols_by_names(df_num_test_kmean.columns.toList :+ "SalePrice")
-  //    .impute_numeric_vars_with_mean_or_median()
+  val df_cat_train_kmean = df_brut_test.select_Impute_And_Transform_categorical_to_numerical()
+    .conveting_multiple_columns_to_specific_DataType(FloatType)
+  val df0 = df_num_train_kmean.join_df2_by_index(df_cat_train_kmean)
 
-  
-  var states = scala.collection.mutable.Map[Int, Double]()
-  val nb_classes = List(2, 3, 5, 8, 20, 30, 40, 150, 200)
-  for (i <- nb_classes) {
-    val df_regularise_trn_tst = regularisation_training_compared_to_testing(df_num_train_kmean, df_num_test_kmean, i)
-    df_regularise_trn_tst.head.show(2, truncate = false)
-    df_regularise_trn_tst(1).show(2, truncate = false)
+  val df_is_test = df_brut_test.select("Id")
 
-    val rf = new RandomForestRegressor().setNumTrees(10).setMaxDepth(5)
-    val rf_mod = rf.fit(df_regularise_trn_tst.head)
+//  var states = scala.collection.mutable.Map[Int, Double]()
+//  val nb_classes = List(2, 3, 5, 8, 20, 30, 200)
+//  for (i <- nb_classes) {
+//    val df_regularise_trn_tst = regularisation_training_compared_to_testing(df0, df1, i)
+//    df_regularise_trn_tst.head.show(2, truncate = false)
+//    df_regularise_trn_tst(1).show(2, truncate = false)
+//
+//    val rf = new RandomForestRegressor().setNumTrees(10).setMaxDepth(5)
+//    val rf_mod = rf.fit(df_regularise_trn_tst.head)
+//
+//    val predictions = rf_mod.transform(df_regularise_trn_tst(2))
+//
+//    val evaluator = new RegressionEvaluator()
+//      .setLabelCol("label")
+//      .setPredictionCol("prediction")
+//      .setMetricName("rmse")
+//    val rmse = evaluator.evaluate(predictions)
+//    states += (i -> rmse)
+//
+//  }
+//  states.foreach(println)
 
-    val predictions = rf_mod.transform(df_regularise_trn_tst(2))
 
-    val evaluator = new RegressionEvaluator()
-      .setLabelCol("label")
-      .setPredictionCol("prediction")
-      .setMetricName("rmse")
-    val rmse = evaluator.evaluate(predictions)
-    states += (i -> rmse)
+    val df_regularise_trn_tst = regularisation_training_compared_to_testing(df0, df1)
 
-  }
-  states.foreach(println)
+      val rf = new RandomForestRegressor().setNumTrees(1000).setMaxDepth(12)
+      val rf_mod = rf.fit(df_regularise_trn_tst.head)
+
+      val predictions = rf_mod.transform(df_regularise_trn_tst(1))
+
+  val dfdf=predictions.join_df2_by_index(df_is_test)//.withColumnRenamed("prediction","SalePrice")select("Id","prediction")
+  dfdf.write.format("csv").save("/home/farid/Bureau/kaggHousPrice/Data/sam.csv")
+
   spark.close()
 }
